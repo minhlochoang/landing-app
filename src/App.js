89e6953d4849d1a502a00eb9e7e79916040295ac
MinhLoc2401 Hoang <minhloc2401.hoang@gmail.com>
2024-02-27 00:06:07 +0700

    edit profile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
+import PrivateRoute from "./components/PrivateRoute";
 
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,15 +17,19 @@ function App() {
     <>
       <Router>
       <Header></Header>
+
       <Routes>
         <Route path="/" element={<Home/>}></Route>
-        <Route path="/profile" element={<Profile/>}></Route>
+        <Route path="/profile" element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>}></Route>
+        </Route>
         <Route path="/offers" element={<Offers/>}></Route>
         <Route path="/sign-in" element={<SignIn/>}></Route>
         <Route path="/sign-up" element={<SignUp/>}></Route>
         <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
       </Routes>
       </Router>
+
       <ToastContainer
         position="top-right"
         autoClose={5000}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,101 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router';
+
+import { getAuth, updateProfile } from 'firebase/auth';
+
+import { toast } from 'react-toastify';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 export default function Profile() {
+  const auth = getAuth()
+  const navigate = useNavigate()
+
+  const [changeDetail, setChangeDetail] = useState(false)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+  const {name, email} = formData
+
+  function onLogout() {
+    auth.signOut()
+    navigate('/')
+  }
+
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+
+   async function onSubmit(e) {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name
+        })
+
+        const docRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(docRef, {
+          name: name
+        })
+      }
+      toast.success('Profile details updated')
+    } catch (error) {
+      toast.error('Could not update the profile details')
+    }
+  }
+
   return (
-    <div>Profile</div>
+    <>
+      <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
+        <h1 className='text-3xl text-center mt-6 font-bold'>My Profile</h1>
+        <div className='w-full md:w-[50%] mt-6 px-3'>
+        <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled = {!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out
+                ${changeDetail && "bg-red-200 focus:bg-red-200"}
+              }`}
+            />
+
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
+
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center ">
+                Do you want to change your name?
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit()
+                    setChangeDetail(!changeDetail)
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? 'Apply change' : 'Edit'}
+                </span>
+              </p>
+              <p
+                className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer"
+                onClick={onLogout}
+              >
+                Sign out
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
   )
 }
