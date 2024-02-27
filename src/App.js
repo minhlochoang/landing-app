38d24b333259c1ea2a67151f105f6c2bd6882fb8
MinhLoc2401 Hoang <minhloc2401.hoang@gmail.com>
2024-02-27 10:47:17 +0700

    create listing UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import CreateListing from "./pages/CreateListing";
 import ForgotPassword from "./pages/ForgotPassword";
-import PrivateRoute from "./components/PrivateRoute";
-
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -27,6 +29,7 @@ function App() {
         <Route path="/sign-in" element={<SignIn/>}></Route>
         <Route path="/sign-up" element={<SignUp/>}></Route>
         <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+        <Route path="/create-listing" element={<CreateListing/>}></Route>
       </Routes>
       </Router>
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
 
 import { getAuth, updateProfile } from 'firebase/auth';
-
-import { toast } from 'react-toastify';
 import { doc, updateDoc } from 'firebase/firestore';
 import { db } from '../firebase';
+
+import { toast } from 'react-toastify';
+import { FcHome } from 'react-icons/fc'
 
 export default function Profile() {
   const auth = getAuth()
@@ -94,6 +96,13 @@ export default function Profile() {
               </p>
             </div>
           </form>
+
+          <button type='submit' className='w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800'>
+            <Link className='flex justify-center items-center'>
+              <FcHome to='/create-listing' className='mr-2 text-2xl'/>
+              Sell or rent your home
+            </Link>
+          </button>
         </div>
       </section>
     </>
