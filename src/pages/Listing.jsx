1c17682e1accaa97f0038503cea20df50f562993
MinhLoc2401 Hoang <minhloc2401.hoang@gmail.com>
2024-03-08 15:09:58 +0700

    add share copy function

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -69,7 +69,7 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
-      {/* <div
+      <div
         className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
         onClick={() => {
           navigator.clipboard.writeText(window.location.href);
@@ -87,7 +87,7 @@ export default function Listing() {
         </p>
       )}
 
-      <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
+      {/* <div className="m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5">
         <div className=" w-full ">
           <p className="text-2xl font-bold mb-3 text-blue-900">
             {listing.name} - ${" "}
